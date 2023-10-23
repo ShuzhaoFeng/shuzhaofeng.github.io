23f4a9c3699387f7a10fe9afddc9e899e494909a
ShuzhaoFeng <shuzhao_feng@outlook.com>
2023-10-23 10:11:41 -0400

    Switch To Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from '@mui/material';
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <Box component='div' className='App'>
+      <Box component='header' className='App-header'>
+        <Box component='a' href="https://vitejs.dev" target="_blank">
+          <Box component='img' src={viteLogo} className="logo" alt="Vite logo" />
+        </Box>
+        <Box component='a' href="https://react.dev" target="_blank">
+          <Box component='img' src={reactLogo} className="logo react" alt="React logo" />
+        </Box>
+        <Box component='p'>
           Hello, I'm Shuzhao Feng, and this will be my personal website.
-        </p>
-        <p>
+        </Box>
+        <Box component='p'>
           I'm actively developing this website in my free time, so please check back later!
-        </p>
-        <a
+        </Box>
+        <Box component='a'
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
-export default App;
+export default App
