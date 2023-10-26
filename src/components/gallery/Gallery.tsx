d11e42fa9be9e18fb23c8dc1d94839d2b38263cd
ShuzhaoFeng <shuzhao_feng@outlook.com>
2023-10-26 15:37:22 -0400

    Update Gallery Styling

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,74 +1,92 @@
 import { Box, Grid, Typography } from "@mui/material";
 import profilePicture from "../../assets/profile_picture.jpg";
-import { RefObject, createRef } from "react";
+import { useEffect, useState } from "react";
 
-const images = [
-  // placeholder images
-  profilePicture,
-  profilePicture,
-  profilePicture,
-  profilePicture,
-  profilePicture,
+const items = [
+  {
+    title: "Item 1",
+    desc: "This is a placeholder description for Item 1",
+    image: profilePicture,
+    color: "#f00",
+  },
+  {
+    title: "Item 2",
+    desc: "This is a placeholder description for Item 2",
+    image: profilePicture,
+    color: "#0f0",
+  },
+  {
+    title: "Item 3",
+    desc: "This is a placeholder description for Item 3",
+    image: profilePicture,
+    color: "#00f",
+  },
+  {
+    title: "Item 4",
+    desc: "This is a placeholder description for Item 4",
+    image: profilePicture,
+    color: "#ff0",
+  },
+  {
+    title: "Item 5",
+    desc: "This is a placeholder description for Item 5",
+    image: profilePicture,
+    color: "#0ff",
+  },
+  {
+    title: "Item 6",
+    desc: "This is a placeholder description for Item 6",
+    image: profilePicture,
+    color: "#f0f",
+  },
 ];
 
 const Gallery = () => {
-  const refMap = new Map<string, RefObject<HTMLInputElement>>();
+  const [timer, setTimer] = useState(0);
+  const [focusedPosition, setFocusedPosition] = useState(0);
 
-  for (let i = 0; i < images.length; i++) {
-    refMap.set(`carousel_radio_${i + 1}`, createRef<HTMLInputElement>());
-  }
-  const refs = Object.fromEntries(refMap);
+  const autoScroll = () => {
+    setTimer(setInterval(() => {
+      setFocusedPosition(focusedPosition => (focusedPosition + 1) % items.length);
+    }, 4000));
+  };
+
+  useEffect(() => {
+    autoScroll();
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
-    <Box className="carousel">
-      {images.map((image, index) =>
-        index === 0 ? (
-          <Box
-            id={`carousel_radio_${index + 1}`}
-            component="input"
-            type="radio"
-            name="position"
-            sx={{
-              gridColumn: `${index + 2} / ${index + 3}`,
-              gridRow: `2 / 3`,
-            }}
-            ref={refs[`carousel_radio_${index + 1}`]}
-            defaultChecked
-            onChange={() => {
-
-            }}
-          />
-        ) : (
-          <Box
-            id={`carousel_radio_${index + 1}`}
-            component="input"
-            type="radio"
-            name="position"
-            sx={{
-              gridColumn: `${index + 2} / ${index + 3}`,
-              gridRow: `2 / 3`,
-            }}
-            ref={refs[`carousel_radio_${index + 1}`]}
-          />
-        )
-      )}
-      <Box component="div" id="gallery">
-        {images.map((image, index) => (
+    <Grid container className="gallery" onMouseLeave={() => {
+        console.log("Mouse left gallery");
+        console.log("Putting timer on");
+        autoScroll()
+        console.log("Timer: " + timer);
+      }} onMouseEnter={() => {
+        console.log("Mouse entered gallery");
+        console.log("Clearing" + timer);
+        clearInterval(timer)
+        console.log("Timer: " + timer);
+        }}>
+      <Grid item xs={12} className="display">
+        {items.map((item, index) => (
           <Box
             component="div"
             className="item"
-            sx= {{
-              '--offset': index + 1,
+            sx={{
+              transform: `rotateY(${
+                -10 * (focusedPosition - index)
+              }deg) translateX(${-300 * (focusedPosition - index)}px)`,
+              zIndex:
+                focusedPosition -
+                Math.max(focusedPosition - index, -focusedPosition + index),
             }}
             onClick={() => {
               console.log("Clicked. Index: " + index);
-              const checkbox = refs[`carousel_radio_${index + 1}`].current;
-              if (checkbox) {
-                if (checkbox.checked) {
-                  // TODO: open new tab.
-                } else {
-                  checkbox.checked = true;
-                }
-              }
+              setFocusedPosition(index);
             }}
           >
             <Box key={index} className="box" p={2}>
@@ -76,9 +94,13 @@ const Gallery = () => {
                 className="imgBx"
                 display="flex"
                 alignItems="center"
-                sx={{ "--gradient-color": "#fff" }}
+                sx={{ "--gradient-color": item.color }}
               >
-                <Box component="img" src={image} alt={`Image ${index + 1}`} />
+                <Box
+                  component="img"
+                  src={item.image}
+                  alt={`Image ${index + 1}`}
+                />
               </Box>
               <Box className="content">
                 <Grid container>
@@ -89,12 +111,12 @@ const Gallery = () => {
                       component="div"
                       textAlign="start"
                     >
-                      Placeholder Title
+                      {item.title}
                     </Typography>
                   </Grid>
                   <Grid item xs={12}>
                     <Typography variant="body2" textAlign="start">
-                      This is a placeholder description.
+                      {item.desc}
                     </Typography>
                   </Grid>
                 </Grid>
@@ -102,8 +124,33 @@ const Gallery = () => {
             </Box>
           </Box>
         ))}
-      </Box>
-    </Box>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        className="navigation"
+        sx={{
+          gridTemplateColumns: `1fr repeat(${items.length}, 30px) 1fr`,
+        }}
+      >
+        {items.map((_item, index) => (
+          <Box
+            id={`carousel_radio_${index + 1}`}
+            component="input"
+            type="radio"
+            name="position"
+            sx={{
+              gridColumn: `${index + 2} / ${index + 3}`,
+              gridRow: `2 / 3`,
+            }}
+            onChange={() => {
+              setFocusedPosition(index);
+            }}
+            checked={index === focusedPosition}
+          />
+        ))}
+      </Grid>
+    </Grid>
   );
 };
 
