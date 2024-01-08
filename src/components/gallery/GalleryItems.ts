de37bb9a6107043a27f13ffde34a3db462f0fbde
ShuzhaoFeng <shuzhao_feng@outlook.com>
2024-01-08 11:32:37 -0500

    Pretting Files

--- src/components/gallery/GalleryItems.ts
+++ src/components/gallery/GalleryItems.ts
@@ -1,30 +1,31 @@
-import JsonPathSerializerIcon from "../../assets/JsonPathSerializerIcon.png";
-import ReactViteTypescript from "../../assets/ReactViteTypescript.png";
+import JsonPathSerializerIcon from '../../assets/JsonPathSerializerIcon.png'
+import ReactViteTypescript from '../../assets/ReactViteTypescript.png'
 
 type GalleryItem = {
-  title: string;
-  desc: string;
-  image: string;
-  color: string;
-  url: string;
-};
+    title: string
+    desc: string
+    image: string
+    color: string
+    url: string
+}
 
 const galleryItems: GalleryItem[] = [
-  {
-    title: "JsonPathSerializer",
-    color: "#90e090",
-    desc: "JsonPathSerializer is a class library that help C# code to build JSON objects incrementally," +
-    "each time with a JsonPath and a value.",
-    image: JsonPathSerializerIcon,
-    url: "https://github.com/ShuzhaoFeng/JsonPathSerializer"
-  },
-  {
-    title: "shuzhaofeng.github.io",
-    color: "#61dbfb",
-    desc: "Yes, I make this website",
-    image: ReactViteTypescript,
-    url: "https://github.com/ShuzhaoFeng/shuzhaofeng.github.io"
-  },
-];
+    {
+        title: 'JsonPathSerializer',
+        color: '#90e090',
+        desc:
+            'JsonPathSerializer is a class library that help C# code to build JSON objects incrementally,' +
+            'each time with a JsonPath and a value.',
+        image: JsonPathSerializerIcon,
+        url: 'https://github.com/ShuzhaoFeng/JsonPathSerializer',
+    },
+    {
+        title: 'shuzhaofeng.github.io',
+        color: '#61dbfb',
+        desc: 'Yes, I make this website',
+        image: ReactViteTypescript,
+        url: 'https://github.com/ShuzhaoFeng/shuzhaofeng.github.io',
+    },
+]
 
-export default galleryItems;
+export default galleryItems
--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,16 +1,16 @@
-import { Box } from "@mui/material";
-import profilePicture from "../../assets/profile_picture.jpg";
+import { Box } from '@mui/material'
+import profilePicture from '../../assets/profile_picture.jpg'
 
 const Profile = () => (
-  <Box component="div" className="profile">
-    <Box className="shadow" />
-    <Box
-      component="img"
-      src={profilePicture}
-      className="image"
-      alt="Profile Picture"
-    />
-  </Box>
-);
+    <Box component='div' className='profile'>
+        <Box className='shadow' />
+        <Box
+            component='img'
+            src={profilePicture}
+            className='image'
+            alt='Profile Picture'
+        />
+    </Box>
+)
 
-export default Profile;
+export default Profile
